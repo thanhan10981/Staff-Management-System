16d8968603ff9877a58332d5cd7279e447b951ee
Han <ghan42568@gmail.com>
2025-11-04 16:40:04 +0700

    Initial commit

---
+++ src/app/core/layout/layout/layout.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { RouterOutlet } from "@angular/router";
+import { Footer } from "../footer/footer";
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-layout',
+  imports: [Header, RouterOutlet, Footer, Sidebar],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss',
+})
+export class Layout {
+
+}
