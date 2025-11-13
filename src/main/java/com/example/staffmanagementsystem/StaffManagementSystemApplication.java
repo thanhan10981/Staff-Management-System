a3975099cfb42321211f3bebb1ed4ed7d0b267e3
Han <ghan42568@gmail.com>
2025-11-13 16:26:24 +0700

    Initial commit for BE_MAU branch

---
+++ src/main/java/com/example/staffmanagementsystem/StaffManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.staffmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffManagementSystemApplication.class, args);
+    }
+
+}
